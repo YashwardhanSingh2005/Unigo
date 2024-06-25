4113a5d3047aa3de8dd3f05ab3d922c7ed837018
Yashwardhan Singh <138315503+YashwardhanSingh2005@users.noreply.github.com>
2024-06-26 03:58:14 +0530

    yash/small changes with text

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,30 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 const loginScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>loginScreen</Text>
+    <View className="flex-1 items-center justify-center bg-[#0A0A0A]">
+      <Text className="text-3xl text-white">loginScreen</Text>
     </View>
   );
 };
 
 export default loginScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#0A0A0A",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "white",
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-    lineHeight: 30,
-    marginTop: -6,
-    marginBottom: 20,
-    textTransform: "uppercase",
-  },
-});
