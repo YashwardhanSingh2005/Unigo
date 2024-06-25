af4bd7a54065ce386051169501bb8074c44cf1c5
Yashwardhan Singh <138315503+YashwardhanSingh2005@users.noreply.github.com>
2024-06-26 03:56:46 +0530

    yash/finally setting up with tailwindcss for ts

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>CollEve!</Text>
+    <View className="flex-1 items-center justify-center bg-[#0A0A0A]">
+      <Text className="text-3xl text-white">CollEve!</Text>
       <Link href="./(auth)/login" style={{ color: "blue" }}>
         {" "}
         Login Click Here!
@@ -16,21 +16,21 @@ const App = () => {
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#0A0A0A",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "white",
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-    lineHeight: 30,
-    marginTop: -6,
-    marginBottom: 20,
-    textTransform: "uppercase",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#0A0A0A",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   text: {
+//     color: "white",
+//     fontSize: 20,
+//     fontWeight: "bold",
+//     textAlign: "center",
+//     lineHeight: 30,
+//     marginTop: -6,
+//     marginBottom: 20,
+//     textTransform: "uppercase",
+//   },
+// });
