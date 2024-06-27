deff59f7cc951aacd231070caf0b53a301f0ec9e
Yashwardhan Singh <138315503+YashwardhanSingh2005@users.noreply.github.com>
2024-06-27 17:09:09 +0530

    yash/onboarding screen changes made

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,72 @@
-import { Text, View } from "react-native";
 import React from "react";
-import { Redirect, router } from "expo-router";
+import { Text, View, StyleSheet, SafeAreaView, Image } from "react-native";
 import CustomButton from "../Components/CustomButton";
+import { useRouter } from "expo-router";
 
 const App = () => {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 items-center  min-h-[85vh] justify-center bg-[#212121]">
-      <Text className="text-3xl text-white">U N I G O</Text>
-      <CustomButton
-        title="Continue with Login"
-        handlepress={() => {
-          router.push("/(auth)/login");
-        }}
-        containerStyles="w-full mt-7"
-      />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.catchphrase}>One app, all Events</Text>
+        <Text style={styles.catchphrase}> Welcome To </Text>
+        <Image
+          source={require("../assets/unigo_logo.png")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Text style={[styles.text, styles.glow]}>
+          U{" "}
+          <Text style={[styles.text, styles.glow]}>
+            N{" "}
+            <Text style={[styles.text, styles.glow]}>
+              I{" "}
+              <Text style={[styles.text, styles.glow]}>
+                G <Text style={[styles.text, styles.glow]}>O</Text>
+              </Text>
+            </Text>
+          </Text>
+        </Text>
+        <CustomButton
+          title="Continue with Email"
+          handlepress={() => {
+            router.push("/(auth)/login");
+          }}
+          containerStyles="w-60 mt-7"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default App;
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#000000",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "85%", // Adjust as needed
+  },
+  text: {
+    color: "white",
+    fontSize: 50,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    textShadowColor: "rgba(255, 255, 255, 0.75)",
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 20,
+  },
+  glow: {
+    textShadowColor: "white", // Adjust glow color as needed
+    textShadowRadius: 30, // Adjust glow radius as needed
+  },
+  catchphrase: {
+    color: "white",
+    fontSize: 20,
+  },
+});
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#0A0A0A",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   text: {
-//     color: "white",
-//     fontSize: 20,
-//     fontWeight: "bold",
-//     textAlign: "center",
-//     lineHeight: 30,
-//     marginTop: -6,
-//     marginBottom: 20,
-//     textTransform: "uppercase",
-//   },
-// });
+export default App;
